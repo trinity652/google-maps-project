545a3d6f515dbeab49ff5d3baf9a3030a3c8d6ad
Abhilasha Jha <abhilashajha@Abhilashas-MacBook-Pro.local>
2019-07-31 01:43:51 +0530

    Changed settings.py accordingly

--- google_maps/google_maps/settings.py
+++ google_maps/google_maps/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'google_maps.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +74,12 @@ WSGI_APPLICATION = 'google_maps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'checkinmap',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '/Applications/MAMP/tmp/mysql/mysql.sock', 
+        'PORT':'8889', 
     }
 }
 
@@ -118,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- google_maps/manage.py
+++ google_maps/manage.py
@@ -3,6 +3,13 @@
 import os
 import sys
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError: 
+    pass
+
+
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'google_maps.settings')
